5b1bf3ed8b737dff666d8087e5339da6996f9942
DDSilverfish <maarten.declercq16@gmail.com>
2024-10-22 19:41:50 +0200

    /giveHeart to another player command added

--- src/main/java/net/intheminecraftgalaxy/itmg/LifeSteal.java
+++ src/main/java/net/intheminecraftgalaxy/itmg/LifeSteal.java
@@ -1,19 +1,29 @@
 package net.intheminecraftgalaxy.itmg;
 
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.command.CommandSource;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
+
+import java.util.Collection;
 
 
 public class LifeSteal {
@@ -57,10 +67,62 @@ public class LifeSteal {
                         killedHealthAttribute.setBaseValue(Math.max(currentMaxHealthKilled - InTheMinecraftGalaxyConfig.heartDamage, InTheMinecraftGalaxyConfig.minHeart));  // Remove one heart, min is 2
                     }
 
-
-
                 }
             }
         });
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, test) -> {
+            registerGiveHeartCommand(dispatcher);
+        });
     }
+    private static void registerGiveHeartCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("giveHeart")
+                .then(CommandManager.argument("playerName", StringArgumentType.string())
+                        .suggests(SUGGEST_PLAYERS)
+                        .executes(context -> {
+                            String playerName = StringArgumentType.getString(context, "playerName");
+                            ServerCommandSource source = context.getSource();
+                            ServerPlayerEntity giver = source.getPlayer();
+                            ServerPlayerEntity receiver = source.getServer().getPlayerManager().getPlayer(playerName);
+
+                            if (giver == null || receiver == null) {
+                                source.sendFeedback(() -> Text.literal("Player not found!"), false);
+                                return 1;
+                            }
+
+                            EntityAttributeInstance giverHealthAttribute = giver.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+                            EntityAttributeInstance receiverHealthAttribute= giver.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+
+                            assert giverHealthAttribute != null;
+                            assert receiverHealthAttribute != null;
+                            double giverCurrentMaxHealth = giverHealthAttribute.getBaseValue();
+                            double receiverCurrentMaxHealth = receiverHealthAttribute.getBaseValue();
+
+                            if(giverCurrentMaxHealth > InTheMinecraftGalaxyConfig.maxHeartItemGen && receiverCurrentMaxHealth < InTheMinecraftGalaxyConfig.maxHeart && receiver.getName() != giver.getName()) {
+                                giverHealthAttribute.setBaseValue(receiverCurrentMaxHealth - 2);
+                                receiverHealthAttribute.setBaseValue(receiverCurrentMaxHealth + 2);
+
+                                giver.sendMessage(Text.literal("You gave 1 heart to " + receiver.getName().getString() + "!"), false);
+                                receiver.sendMessage(Text.literal("You received 1 heart from " + giver.getName().getString() + "!"), false);
+
+                            } else if (receiver.getName() != giver.getName()) {
+                                giver.sendMessage(Text.literal("You can only send a heart to another player!"), false);
+                            } else if (giverCurrentMaxHealth <= InTheMinecraftGalaxyConfig.maxHeartItemGen) {
+                                giver.sendMessage(Text.literal("You don't have enough health to give a heart!"), false);
+                            }
+                            else
+                            {
+                                giver.sendMessage(Text.literal(receiver.getName().getString() + " already has the maximum amount of hearts!!"), false);
+                            }
+
+                            return 1;
+                        })));
+    }
+
+    // Suggestion provider for players
+    private static final SuggestionProvider<ServerCommandSource> SUGGEST_PLAYERS = (context, builder) -> {
+        Collection<ServerPlayerEntity> players = context.getSource().getServer().getPlayerManager().getPlayerList();
+        return CommandSource.suggestMatching(players.stream().map(player -> player.getGameProfile().getName()), builder);
+    };
+
 }
